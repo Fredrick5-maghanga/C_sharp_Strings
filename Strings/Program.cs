eda40d4711c9795cb52942ead5ddf35f73d51a92
Fredrick5-maghanga <maghangafredrick5@gmail.com>
2023-04-04 08:34:48 +0300

    Add project files.

---
+++ Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Strings
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Utilities.ManipulatingStrings();
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Strings/Utilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    public class Utilities
+    {
+        //Manipulating  Strings
+        public static void ManipulatingStrings()
+        {
+            string firstName = "Bethany";
+
+            string lastName = "Smith";
+            string fullName = firstName + " " + lastName;
+            string employeeIdentification= string.Concat(firstName, lastName);
+            string empId = firstName.ToLower() + "-" + lastName.ToLower();
+            int length=empId.Length;
+
+            if (fullName.Contains ("beth") || fullName.Contains("Beth"))
+            {
+                Console.WriteLine("It's Bethany!");
+            }
+            string subString= fullName.Substring (1,3);
+            Console.WriteLine("Characters 2 to 4 of fullName are" +" " + subString);
+            string userNameWithInterpolation = $"{firstName}-{lastName}";
+        }
+
+        public static void UsingSimmpleStrings()
+        {
+            string firstName = "Bethany";
+            
+            string lastName = "Smith";
+           
+            string s;
+            s = firstName;
+            var userName = "BethanyS";
+            userName = userName.ToLower();
+
+            userName = "";//identical to string.Empty;
+
+        }
+    }
+}
